08e698fd8daba755b6f352d43b23eb45f3b6e5ed
haikh <haikh@bap.jp>
2019-05-07 08:54:57 +0700

    deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,9 @@ set :linked_files, %w(config/database.yml config/application.yml)
 set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads)
 set :keep_releases, 5
 set :rvm_type, :user
+set :use_sudo, true
 set :branch, "master"
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 set :puma_rackup, -> {File.join(current_path, "config.ru")}
 set :puma_state, -> {"#{shared_path}/tmp/pids/puma.state"}
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,7 @@ set :rails_env, :production
 set :deploy_to, "/deploy/apps/blog"
 set :branch, "master"
 server "13.229.136.10", user: "ubuntu", roles: %w(web app db)
+set :ssh_options, { forward_agent: true }
 
 # server-based syntax
 # ======================
