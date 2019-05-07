07acc2a0a27da3a1b3d05c22d2e4ca15c8be1a8b
haikh <haikh@bap.jp>
2019-05-07 14:56:44 +0700

    Set up Puma, Nginx & Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,25 @@
-server "13.229.136.10", user: "ubuntu", roles: %w(web app db)
-
 set :repo_url, "git@github.com:kyhuuhai/api-rails-react-demo.git"
 set :application,     'blog'
-set :user,            'ubuntu'
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app, true
+set :puma_rackup, -> {File.join(current_path, "config.ru")}
+set :puma_state, -> {"#{shared_path}/tmp/pids/puma.state"}
+set :puma_pid, -> {"#{shared_path}/tmp/pids/puma.pid"}
+set :puma_bind, -> {"unix://#{shared_path}/tmp/sockets/puma.sock"}
+set :puma_conf, -> {"#{shared_path}/puma.rb"}
+set :puma_access_log, -> {"#{shared_path}/log/puma_access.log"}
+set :puma_error_log, -> {"#{shared_path}/log/puma_error.log"}
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, "staging"))
+set :puma_threads, [0, 8]
+set :puma_workers, 0
 set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :puma_init_active_record, true
+set :puma_preload_app, false  # Change to false when not using ActiveRecord
 
 ## Defaults:
 # set :scm,           :git
