863a5135ca6e04a974236e7a867908ec6a1c34f7
haikh <haikh@bap.jp>
2019-05-07 09:47:56 +0700

    deploy config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,10 @@
+set :stage, :production
+set :rails_env, :production
+set :deploy_to, "/deploy/apps/blog"
+set :branch, "master"
+server "13.229.136.10", user: "ubuntu", roles: %w(web app db)
+set :ssh_options, { forward_agent: true }
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
